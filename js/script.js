9d80713dce8d19782209c845013feaae98afe7b0
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-10 10:51:32 +0000

    sdded styling and new js code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-$('p').click(function() {
-    $('p').slideToggle('slow');
-});
+$('document').ready(function() {
+    $('.box').on('click', function() {
+        var classNames = $(this).attr('class').split(' ');
+        $('.' + classNames[1]).css('background-color', 'red');
+   })
+}) /** when you click on a box with class it identifies all boxes with same clas using 'this' keyword and changes css to all */
